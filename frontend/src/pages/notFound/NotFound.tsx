3a4d617427cc512ddacd2742fc490bb46c005ba8
alibomba <wojci.bro@gmail.com>
2024-01-02 12:03:08 +0100

    not found page

--- frontend/src/pages/notFound/NotFound.tsx
+++ frontend/src/pages/notFound/NotFound.tsx
@@ -1,12 +1,13 @@
-
+import { Link } from 'react-router-dom';
 
 
 import styles from './notFound.module.css';
 
 const NotFound = () => {
     return (
-        <div>
-
+        <div className={styles.main}>
+            <p className={styles.main__status}>404</p>
+            <Link to='/' className={styles.main__button}>Strona Główna</Link>
         </div>
     )
 }
